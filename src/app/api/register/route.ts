68fb1e9d05d226c9c941bd9ad0897f4de29de078
Precioussoul <habsoff@gmail.com>
2023-10-03 17:53:04 +0100

    feat: working on /api/register endpoint

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,20 +3,27 @@ import prisma from "@/libs/prismaDb"
 import {NextResponse} from "next/server"
 
 export async function POST(request: Request) {
-  const body = await request.json()
-  const {email, name, password} = body
+  try {
+    const body = await request.json()
+    const {email, name, password} = body
 
-  if (!email || !name || !password) {
-    return new NextResponse("Missing Information", {status: 400})
+    if (!email || !name || !password) {
+      return new NextResponse("Missing Information", {status: 400})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+      },
+    })
+
+    return NextResponse.json(user)
+  } catch (error) {
+    console.log(error, "REGISTRATION ERROR")
+    return new NextResponse("Internal Server Error", {status: 500})
   }
-
-  const hashedPassword = await bcrypt.hash(password, 12)
-
-  const user = await prisma.user.create({
-    data: {
-      email,
-      name,
-      hashedPassword,
-    },
-  })
 }
