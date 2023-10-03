4806870cdd19310c686b8d7c6b586b82b1e77b74
Precioussoul <habsoff@gmail.com>
2023-10-03 22:42:00 +0100

    feat: added custom hooks

--- src/hooks/useRoutes.ts
+++ src/hooks/useRoutes.ts
@@ -1,6 +1,6 @@
 import {useMemo} from "react"
 import {usePathname} from "next/navigation"
-import {HiChat} from "react-icons/hi"
+import {HiChat, HiUsers} from "react-icons/hi"
 import {HiArrowLeftOnRectangle, HiUser} from "react-icons/hi2"
 import {signOut} from "next-auth/react"
 
@@ -9,4 +9,28 @@ import useConversation from "./useConversation"
 const useRoutes = () => {
   const pathname = usePathname()
   const {conversationId} = useConversation()
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Chat",
+        href: "/conversations",
+        icons: HiChat,
+        active: pathname === "/conversations" || !!conversationId,
+      },
+      {
+        label: "Users",
+        href: "/users",
+        icons: HiUsers,
+        active: pathname === "/users",
+      },
+      {
+        label: "Logout",
+        href: "#",
+        icons: HiArrowLeftOnRectangle,
+        onClick: () => signOut(),
+      },
+    ],
+    [pathname, conversationId]
+  )
 }
