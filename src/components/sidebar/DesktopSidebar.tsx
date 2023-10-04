cfb80e704170d8813f388f2f56c5008f7da51316
Precioussoul <habsoff@gmail.com>
2023-10-04 21:55:03 +0100

    feat: installed next-superjson plugin

--- src/components/sidebar/DesktopSidebar.tsx
+++ src/components/sidebar/DesktopSidebar.tsx
@@ -2,10 +2,17 @@
 import useRoutes from "@/hooks/useRoutes"
 import React, {useState} from "react"
 import DesktopItem from "./DesktopItem"
+import {User} from "@prisma/client"
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User
+}
+
+const DesktopSidebar = ({currentUser}: DesktopSidebarProps) => {
   const routes = useRoutes()
   const [isOpen, setisOpen] = useState()
+
+  console.log({currentUser})
 
   return (
     <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-5 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between'>
