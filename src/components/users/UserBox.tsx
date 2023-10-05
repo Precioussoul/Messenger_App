63cd7729e49b3adcd1f25040fe55443b54f6dae5
Precioussoul <habsoff@gmail.com>
2023-10-05 22:52:18 +0100

    feat: working on userBox component

--- src/components/users/UserBox.tsx
+++ src/components/users/UserBox.tsx
@@ -1,7 +1,38 @@
-import React from "react"
+"use client"
+import {User} from "@prisma/client"
+import axios from "axios"
+import {useRouter} from "next/navigation"
+import React, {useCallback, useState} from "react"
+import Avatar from "../Avatar"
 
-const UserBox = () => {
-  return <div>UserBox</div>
+interface UserBoxProps {
+  data: User
+}
+
+const UserBox = ({data}: UserBoxProps) => {
+  const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleClick = useCallback(() => {
+    setIsLoading(true)
+    axios
+      .post("/api/conversations", {
+        userId: data.id,
+      })
+      .then((data) => {
+        router.push(`/conversations/${data?.data?.id}`)
+      })
+  }, [data, router])
+
+  return (
+    <div
+      onClick={handleClick}
+      className='w-full relative flex items-center space-x-3 bg-white p-3
+       hover:bg-neutral-100 rounded-lg transition cursor-pointer '
+    >
+      <Avatar user={data} />
+    </div>
+  )
 }
 
 export default UserBox
