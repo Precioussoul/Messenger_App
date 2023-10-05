7564b212319c8b950d9c3d347625fb9476bc26e7
Precioussoul <habsoff@gmail.com>
2023-10-05 22:57:57 +0100

    feat: worked on userBox component

--- src/components/users/UserBox.tsx
+++ src/components/users/UserBox.tsx
@@ -31,6 +31,13 @@ const UserBox = ({data}: UserBoxProps) => {
        hover:bg-neutral-100 rounded-lg transition cursor-pointer '
     >
       <Avatar user={data} />
+      <div className='min-w-0 flex-1'>
+        <div className='focus:outline-none'>
+          <div className='flex justify-between items-center mb-1'>
+            <p className='text-sm font-medium text-gray-900'>{data?.name}</p>
+          </div>{" "}
+        </div>
+      </div>
     </div>
   )
 }
